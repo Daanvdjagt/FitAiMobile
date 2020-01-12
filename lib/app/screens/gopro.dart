ab6660c23efc7029a6d488ae715a567afc4a512b
David <davidas98_@hotmail.com>
2020-01-12 23:19:33 +0100

    Changed layout gopro screen

--- lib/app/screens/gopro.dart
+++ lib/app/screens/gopro.dart
@@ -10,6 +10,37 @@ class Gopro extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('FitAIMobile')),
       drawer: Navbar("Gopro"),
+      body: Center(
+        child: Container(
+          child: Center(
+            child: Text(
+            "Upgrade to pro to get the most of the app!",
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontWeight: FontWeight.bold)
+            ),
+          ),
+          width: 300, 
+          height: 400,
+          decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          boxShadow: [
+            BoxShadow(
+            color: Colors.grey,
+            blurRadius: 20.0,
+            spreadRadius: 5.0,     
+            )
+          ],
+          color: Colors.white,
+          border: Border.all(
+            width: 1.0,
+            color: Colors.grey,
+            )
+          
+          ),
+        )
+      ,),
+      
     );
   }
 }
